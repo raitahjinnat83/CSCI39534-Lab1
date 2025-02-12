f66cfc8f6406ccd288087908cf803ff2b5f9caa6
Raitah Jinnat <raitahjinnat@gmail.com>
2025-02-12 12:11:09 -0500

    PIL imported

--- ColorProcessing.py
+++ ColorProcessing.py
@@ -4,12 +4,17 @@
 # import packagaes for images and arrays
 import matplotlib.pyplot as plt
 import numpy as numpy
+import PIL
 
 input = "dog.png"
 
 dog = plt.imread(input) # Read in image from the input
 plt.imshow(dog) # Load image into pyplot
 plt.show() # Show the image
+
+# Single Color Extraction
+
+
 
 
 
